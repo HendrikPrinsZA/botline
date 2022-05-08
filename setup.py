d6a96b7ee3c187c75b19b42fb084b83f648af0c6
Hendrik Prinsloo <info@hendrikprinsloo.co.za>
2022-05-08 22:46:35 +0200

    Changed license to MIT

--- setup.py
+++ setup.py
@@ -1,27 +1,34 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 import setuptools
 
 with open('README.md') as f:
-  long_description = f.read()
+    long_description = f.read()
 
 with open('LICENSE') as f:
-  license = f.read()
+    license = f.read()
 
 def parse_requirements(filename):
-  lines = (line.strip() for line in open(filename))
-  return [line for line in lines if line and not line.startswith("#")]
+    lines = (line.strip() for line in open(filename))
+    return [line for line in lines if line and not line.startswith("#")]
 
 setup(
-  name = 'botline',
-  version = '0.0.1',
-  description = 'Call a bot and have a conversation',
-  long_description = long_description,
-  long_description_content_type = "text/markdown",
-  author = 'Hendrik Prinsloo',
-  author_email = 'info@hendrikprinsloo.co.za',
-  url = 'https://github.com/HendrikPrinsZA/botline',
-  license = license,
-  packages= setuptools.find_packages(),
-  install_requires = parse_requirements('requirements.txt'),
+    name = 'botline',
+    version = '0.0.1',
+    description = 'Call a bot and have a conversation',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    author = 'Hendrik Prinsloo',
+    author_email = 'info@hendrikprinsloo.co.za',
+    url = 'https://github.com/HendrikPrinsZA/botline',
+    license = license,
+    packages= setuptools.find_packages(),
+    install_requires = parse_requirements('requirements.txt'),
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    py_modules=['botline'],
 )
 
